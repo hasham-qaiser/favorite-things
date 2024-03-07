07ce68460f7bdefeb95006ba45c9bd34611d1a75
Hasham Qaiser <hashamqaiser24@gmail.com>
2024-03-06 17:12:24 -0800

    added items

--- app/page.tsx
+++ app/page.tsx
@@ -15,17 +15,51 @@ export default function Home() {
       <div className="grid grid-cols-4 gap-20">
         <div className="flex flex-col items-center">
           <Card className="w-[300px]">
-            <CardContent>
+            <CardContent className="h-48 object-cover">
               <Image
-                src={"/images/nextjs.svg"}
+                className="rounded-lg mt-4"
+                src={"/images/macbookpro.jpeg"}
                 alt="Next.js Logo"
                 width={300}
                 height={300}
               />
             </CardContent>
             <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
+              <CardTitle>Macbook Pro 14 - Inch</CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <Card className="w-[300px]">
+            <CardContent className="h-48 object-cover">
+              <Image
+                className="rounded-lg mt-4 object-cover"
+                src={"/images/airpodspro.jpeg"}
+                alt="Next.js Logo"
+                width={300}
+                height={300}
+              />
+            </CardContent>
+            <CardHeader>
+              <CardTitle>Airpods Pro</CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <Card className="w-[300px]">
+            <CardContent className="h-48 object-cover">
+              <Image
+                className="rounded-lg mt-4 object-cover"
+                src={"/images/iphone15pro.jpeg"}
+                alt="Next.js Logo"
+                width={400}
+                height={400}
+              />
+            </CardContent>
+            <CardHeader>
+              <CardTitle>Airpods Pro</CardTitle>
             </CardHeader>
           </Card>
         </div>

61d3201928e64c8306b8817cdd7544f3d260587a
Hasham Qaiser <hashamqaiser24@gmail.com>
2024-03-06 20:35:12 -0800

    styles changed new png images added

--- app/page.tsx
+++ app/page.tsx
@@ -12,56 +12,35 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
-      <div className="grid grid-cols-4 gap-20">
-        <div className="flex flex-col items-center">
-          <Card className="w-[300px]">
-            <CardContent className="h-48 object-cover">
+      <div className="grid grid-cols-4 gap-8">
+        <div>
+          <Card>
+            <CardContent className="flex flex-col items-center justify-between">
               <Image
                 className="rounded-lg mt-4"
-                src={"/images/macbookpro.jpeg"}
+                src={"/images/macbook-pro.png"}
                 alt="Next.js Logo"
                 width={300}
                 height={300}
               />
             </CardContent>
-            <CardHeader>
-              <CardTitle>Macbook Pro 14 - Inch</CardTitle>
-            </CardHeader>
           </Card>
+          <p className="text-center mt-2">Macbook Pro</p>
         </div>
 
-        <div className="flex flex-col items-center">
-          <Card className="w-[300px]">
-            <CardContent className="h-48 object-cover">
+        <div>
+          <Card>
+            <CardContent className="flex flex-col items-center">
               <Image
-                className="rounded-lg mt-4 object-cover"
-                src={"/images/airpodspro.jpeg"}
+                className="rounded-lg"
+                src={"/images/airpod-pro.png"}
                 alt="Next.js Logo"
                 width={300}
                 height={300}
               />
             </CardContent>
-            <CardHeader>
-              <CardTitle>Airpods Pro</CardTitle>
-            </CardHeader>
           </Card>
-        </div>
-
-        <div className="flex flex-col items-center">
-          <Card className="w-[300px]">
-            <CardContent className="h-48 object-cover">
-              <Image
-                className="rounded-lg mt-4 object-cover"
-                src={"/images/iphone15pro.jpeg"}
-                alt="Next.js Logo"
-                width={400}
-                height={400}
-              />
-            </CardContent>
-            <CardHeader>
-              <CardTitle>Airpods Pro</CardTitle>
-            </CardHeader>
-          </Card>
+          <p className="text-center mt-2">Airpods Pro 2</p>
         </div>
       </div>
     </div>
